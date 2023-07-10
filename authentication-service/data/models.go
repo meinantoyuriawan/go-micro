1a1942e5ffaa89c4d6feff8ca5a3f79c31b41674
meinantoyuriawan <meinantoyuriawan@gmail.com>
2023-07-10 15:40:09 +0700

    init auth commit

---
+++ authentication-service/data/models.go
@@ -0,0 +1,24 @@
+package data
+
+import (
+	"time"
+)
+
+// Models is the type for this package. Note that any model that is included as a member
+// in this type is available to us throughout the application, anywhere that the
+// app variable is used, provided that the model is also added in the New function.
+type Models struct {
+	User User
+}
+
+// User is the structure which holds one user from the database.
+type User struct {
+	ID        int       `json:"id"`
+	Email     string    `json:"email"`
+	FirstName string    `json:"first_name,omitempty"`
+	LastName  string    `json:"last_name,omitempty"`
+	Password  string    `json:"-"`
+	Active    int       `json:"active"`
+	CreatedAt time.Time `json:"created_at"`
+	UpdatedAt time.Time `json:"updated_at"`
+}
